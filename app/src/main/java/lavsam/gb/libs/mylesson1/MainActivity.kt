9f660b1229f8885121b474ef46074ba4a3e1633e
lavsam-dev <lavsam@gmail.com>
2021-10-11 23:30:32 +0400

    Initial commit

---
+++ app/src/main/java/lavsam/gb/libs/mylesson1/MainActivity.kt
@@ -0,0 +1,34 @@
+package lavsam.gb.libs.mylesson1
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import lavsam.gb.libs.mylesson1.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), MainView {
+
+    private lateinit var mainBinding: ActivityMainBinding
+
+    private val presenter = MainPresenter(this)
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        mainBinding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(mainBinding.root)
+        val listener = View.OnClickListener {
+            presenter.counterClick(it.id)
+        }
+        mainBinding.btnCounter1.setOnClickListener(listener)
+        mainBinding.btnCounter2.setOnClickListener(listener)
+        mainBinding.btnCounter3.setOnClickListener(listener)
+    }
+
+    //Подсказка к ПЗ: поделить на 3 отдельные функции и избавиться от index
+    override fun setButtonText(index: Int, text: String) {
+        when (index) {
+            0 -> mainBinding.btnCounter1.text = text
+            1 -> mainBinding.btnCounter2.text = text
+            2 -> mainBinding.btnCounter3.text = text
+        }
+    }
+}
---
+++ app/src/main/java/lavsam/gb/libs/mylesson1/MainView.kt
@@ -0,0 +1,5 @@
+package lavsam.gb.libs.mylesson1
+
+interface MainView {
+    fun setButtonText(index: Int, text: String)
+}
