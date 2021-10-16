6acc3aed70e8455466185067a346bace56d613ed
lavsam-dev <lavsam@gmail.com>
2021-10-16 14:06:03 +0400

    Moxy + три метода вместо одного

--- app/src/main/java/lavsam/gb/libs/mylesson1/IMainView.kt
+++ app/src/main/java/lavsam/gb/libs/mylesson1/IMainView.kt
@@ -1,5 +1,13 @@
 package lavsam.gb.libs.mylesson1
 
-interface IMainView {
-    fun setButtonText(type: CounterType, text: String)
+import moxy.MvpView
+import moxy.viewstate.strategy.AddToEndSingleStrategy
+import moxy.viewstate.strategy.StateStrategyType
+
+//@AddToEndSingle() //- можно такой алиас
+@StateStrategyType(AddToEndSingleStrategy::class)
+interface IMainView : MvpView {
+    fun setButtonOneText(text: String)
+    fun setButtonTwoText(text: String)
+    fun setButtonThreeText(text: String)
 }
--- app/src/main/java/lavsam/gb/libs/mylesson1/MainPresenter.kt
+++ app/src/main/java/lavsam/gb/libs/mylesson1/MainPresenter.kt
@@ -5,12 +5,11 @@ class MainPresenter(private val view: IMainView) {
     private val model = CountersModel()
 
     fun counterClick(type: CounterType) {
-        val nextValue = when (type) {
-            CounterType.COUNTER_OF_DAYS -> model.next(0)
-            CounterType.COUNTER_OF_YEARS -> model.next(1)
-            CounterType.COUNTER_OF_PAYLOAD -> model.next(2)
+        when (type) {
+            CounterType.COUNTER_OF_DAYS -> view.setButtonOneText(model.next(0).toString())
+            CounterType.COUNTER_OF_YEARS -> view.setButtonTwoText(model.next(1).toString())
+            CounterType.COUNTER_OF_PAYLOAD -> view.setButtonThreeText(model.next(2).toString())
         }
-        view.setButtonText(type, nextValue.toString())
     }
 }
 
